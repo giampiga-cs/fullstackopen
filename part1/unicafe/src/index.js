b894353b75594de8a7b4fb45992a2686b67099ac
giampiga-cs <78252077+giampiga-cs@users.noreply.github.com>
2021-03-09 04:11:19 -0500

    1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,11 +1,11 @@
-//unicafe step 1, 1.6
+//unicafe step 2, 1.7
 
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Display = props => <div>{props.text}
-                              {props.value
-                        }</div>
+                              {props.value}
+                          </div>
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
@@ -19,29 +19,21 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const incGood = newValue => {
-    setGood(newValue);
-    console.log("x");
-  }
-
-  const incNeutral = newValue => {
-    setNeutral(newValue);
-  }
-
-  const incBad = newValue => {
-    setBad(newValue);
-  }
+  // Showed how to do it without using a Display component(?)
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => incGood(good + 1)} text="good" />
-      <Button handleClick={() => incNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => incBad(bad + 1)} text="bad" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
       <Display text="good " value={good} />
       <Display text="neutral " value={neutral} />
       <Display text="bad " value={bad} />
+      <Display text="all " value={good + neutral + bad} />
+      <Display text="average " value={(good - bad) / (good + neutral + bad)} />
+      <p>positive {good / (good + neutral + bad) * 100} %</p>
     </div>
   )
 }
