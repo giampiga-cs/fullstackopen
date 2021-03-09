a5a39eb793a6c0ab5105f9957fed6b727c1030bd
giampiga-cs <78252077+giampiga-cs@users.noreply.github.com>
2021-03-09 04:12:56 -0500

    1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,11 +1,7 @@
-//unicafe step 2, 1.7
+//unicafe step 3, 1.8
 
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
-const Display = props => <div>{props.text}
-                              {props.value}
-                          </div>
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
@@ -13,13 +9,27 @@ const Button = (props) => (
   </button>
 )
 
+// Did with Statistic component and HTML
+const Statistic = (props) => {
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.good + props.neutral + props.bad}</p>
+      <p>average {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</p>
+      <p>positive {props.good / (props.good + props.neutral + props.bad) * 100}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  // Showed how to do it without using a Display component(?)
+  // Showed how to do it with a Statistics component
 
   return (
     <div>
@@ -28,12 +38,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Display text="good " value={good} />
-      <Display text="neutral " value={neutral} />
-      <Display text="bad " value={bad} />
-      <Display text="all " value={good + neutral + bad} />
-      <Display text="average " value={(good - bad) / (good + neutral + bad)} />
-      <p>positive {good / (good + neutral + bad) * 100} %</p>
+      <Statistic good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
