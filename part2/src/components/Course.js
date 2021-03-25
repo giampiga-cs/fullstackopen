73e38c24117285eb73c9af93288f2d41dcfbf376
giampiga-cs <78252077+giampiga-cs@users.noreply.github.com>
2021-03-25 12:13:55 -0400

    Add files via upload

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -7,6 +7,7 @@ const Course = ({course}) => {
       <>
         <Header name={course.name}/>
         <Content parts={course.parts} />
+        <b>total of {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises} exercises</b>
       </>
     )
 }
